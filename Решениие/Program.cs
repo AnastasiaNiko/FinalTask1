0527cf63d3ac9d4f43dbdc3b37eac77751e6de2b
Анастасия Николаева <nikolaeva@MacBook-Pro-Anastasia.local>
2023-09-06 00:24:14 +0300

    Решение

---
+++ Решениие/Program.cs
@@ -0,0 +1,34 @@
+﻿ string[] lenghtArray = { "hello", "day", "04", "sept", "2023", ":-)" };
+
+    string[] array1 = new string[lenghtArray];
+
+    int maxLength = 3;
+
+    void CutArray(string[] array, string[] array1)
+    {
+        int count = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Length <= maxLength)
+            {
+                array1[count] = array[i];
+                count++;
+            }
+        }
+    }
+    void PrintArray(string[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write($" {array[i]} ");
+        }
+        Console.WriteLine();
+    }
+    CutArray(lenghtArray, array1);
+    Console.Write( $"Введен  массив: {lenghtArray}");
+    PrintArray(lenghtArray);
+    Console.Write($"Элементы массива <= 3 символам: ");
+    PrintArray(array1);
+
+
+
